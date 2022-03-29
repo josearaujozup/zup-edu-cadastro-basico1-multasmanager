13b163e595c685b8f5827bf998698e5897161e3e
Denes <jose.araujo@zup.com.br>
2022-03-29 09:09:55 -0300

    criado o projeto de gerenciar multas

---
+++ src/main/java/br/com/zup/edu/multasmanager/MultasmanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.multasmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultasmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultasmanagerApplication.class, args);
+	}
+
+}
